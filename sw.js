8c9cae266f05e5823198324667873cb9b1385751
Chefrel Baracol <chefrelbarac@gmail.com>
2026-08-10 19:04:21 +0800

    added copy product feature

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX = 'costing-cache';
-const CACHE_NAME = PREFIX + '-v2.0.10';
+const CACHE_NAME = PREFIX + '-v2.0.11';
 
 // Optional: pre-cache a few known static assets on install.
 // Not required for SWR to work — it'll cache things on first fetch anyway.
